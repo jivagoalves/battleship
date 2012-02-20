2aa9911039992bba1fe3f2910142f21904d63dbc
Jivago Alves <jivagoalves@gmail.com>
2012-02-20 17:32:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battleship::Application.config.secret_token = '200b2dc6f94da9e50c42648afd5844c56e533952d117f92cd473d57ec9ec341bba7343e5cc41a45f2a6424d777812d7f77c11c3abd5d32c6be25c36eacaea11d'
